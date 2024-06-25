727165813bb12a438413a23b6d1671c155164869
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-06-25 08:15:51 -0300

    :sparkles: Primeiro Commit: Construir o projeto Spring com todas as dependências

---
+++ src/test/java/com/fatec/cidade/CidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.cidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
