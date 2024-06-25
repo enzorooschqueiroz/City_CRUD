1296bd3bd8c55ac92e1a62d0a959932ace56612e
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-06-25 09:32:07 -0300

    :sparkles: Sexto Commit : Implementar todo o end-point GET /cities/{id}

--- src/main/java/com/fatec/cidade/resources/CidadeController.java
+++ src/main/java/com/fatec/cidade/resources/CidadeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class CidadeController {
     @GetMapping
     public List<Cidade> getCidades(){
         return cidadeService.getCidades();
+    }
+
+    @GetMapping({"{id}"})
+    public Cidade getCidadeById(@PathVariable int id){
+        return cidadeService.getCidadeById(id);
+      
     }
     
 }
--- src/main/java/com/fatec/cidade/services/CidadeService.java
+++ src/main/java/com/fatec/cidade/services/CidadeService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.fatec.cidade.entities.Cidade;
 import com.fatec.cidade.repositories.CidadeRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class CidadeService {
 
@@ -16,6 +18,12 @@ public class CidadeService {
 
     public List<Cidade> getCidades(){
         return cidadeRepository.findAll();
+    }
+
+    public Cidade getCidadeById(int id){
+        return cidadeRepository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Cidade não Cadastrada")
+        );
     }
     
 }
